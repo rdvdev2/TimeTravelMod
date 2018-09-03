c923f1c63836234d2a36722fd54c156430e1110f
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2018-09-03 02:50:27 +0200

    Remove old GUI code

--- src/main/java/com/rdvdev2/TimeTravelMod/client/gui/GuiTimeMachine.java
+++ src/main/java/com/rdvdev2/TimeTravelMod/client/gui/GuiTimeMachine.java
@@ -43,19 +43,6 @@ public class GuiTimeMachine extends GuiScreen {
 
     @Override
     public void initGui() {
-        /*
-        ButtonPresent = new GuiButton(0, this.width / 2 -100, (this.height / (buttons+1))*1, I18n.format("gui.tm.present.text"));
-        ButtonOldWest = new GuiButton(1, this.width / 2 -100, (this.height / (buttons+1))*2, I18n.format("gui.tm.oldwest.text"));
-        this.buttonList.add(ButtonPresent);
-        this.buttonList.add(ButtonOldWest);
-
-        switch (tm.getTier()){
-            case 0:
-                ButtonOldWest.enabled=false;
-            case 1:
-                break;
-        }
-        */
         tls = timeLines.getTimeLines();
         TimeLine[] atls = timeLines.getAvailableTimeLines(tm.getTier());
         int buttoncount = tls.length+1;
@@ -89,23 +76,6 @@ public class GuiTimeMachine extends GuiScreen {
     @Override
     public void actionPerformed(GuiButton button) throws IOException {
         this.mc.displayGuiScreen(null);
-        /*
-        int id;
-        if (button == ButtonPresent) {
-            id = 0;
-        }
-        else if (button == ButtonOldWest) {
-            id = ModTimeLines.oldWest.getDimId();
-        } else {
-            id = -1;
-        }
-
-        if (id != player.dimension && player.dimension != 1 && player.dimension != -1) {
-            ModPacketHandler.INSTANCE.sendToServer(new DimensionTP(id, tm, pos, side));
-        } else {
-            this.sendChatMessage(I18n.format("gui.tm.error.text"), false);
-        }
-        */
         int id;
         if (button.id == 0) {
             id = 0;
