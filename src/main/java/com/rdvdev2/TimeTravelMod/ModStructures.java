71c2f6179cfc34cc0910073d301ac7f60a9e36eb
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2018-10-28 13:26:11 +0100

    Old West Villages work

--- src/main/java/com/rdvdev2/TimeTravelMod/ModStructures.java
+++ src/main/java/com/rdvdev2/TimeTravelMod/ModStructures.java
@@ -1,10 +1,12 @@
 package com.rdvdev2.TimeTravelMod;
 
 import com.rdvdev2.TimeTravelMod.common.dimension.oldwest.village.MapGenVillageOldWest;
+import com.rdvdev2.TimeTravelMod.common.dimension.oldwest.village.StructureVillageOldWestPieces;
 import net.minecraft.world.gen.structure.MapGenStructureIO;
 
 public class ModStructures {
     public static void init() {
         MapGenStructureIO.registerStructure(MapGenVillageOldWest.Start.class, "timetravelmod:oldwestvillage");
+        StructureVillageOldWestPieces.registerVillageOldWestPieces();
     }
 }
--- src/main/java/com/rdvdev2/TimeTravelMod/common/dimension/oldwest/village/StructureVillageOldWestPieces.java
+++ src/main/java/com/rdvdev2/TimeTravelMod/common/dimension/oldwest/village/StructureVillageOldWestPieces.java
@@ -1,10 +1,7 @@
 package com.rdvdev2.TimeTravelMod.common.dimension.oldwest.village;
 
-import net.minecraft.world.gen.structure.MapGenStructureIO;
-import net.minecraft.world.gen.structure.StructureVillagePieces;
-
 public class StructureVillageOldWestPieces {
     public static void registerVillageOldWestPieces() {
-        MapGenStructureIO.registerStructureComponent(StructureVillagePieces.Path.class, "ViSR");
+        // Register the village pieces here
     }
 }
