97408988df434df864387ad59292422f0ec42ecc
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2018-11-01 00:31:31 +0100

    Override functions of the vanilla cauldron to avoid the Temporal Cauldron to behave like the vanilla one

--- src/main/java/com/rdvdev2/TimeTravelMod/common/block/BlockTemporalCauldron.java
+++ src/main/java/com/rdvdev2/TimeTravelMod/common/block/BlockTemporalCauldron.java
@@ -1,7 +1,19 @@
 package com.rdvdev2.TimeTravelMod.common.block;
 
+import com.rdvdev2.TimeTravelMod.ModBlocks;
 import com.rdvdev2.TimeTravelMod.TimeTravelMod;
 import net.minecraft.block.BlockCauldron;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockTemporalCauldron extends BlockCauldron {
     private String name = "temporalcauldron";
@@ -11,5 +23,40 @@ public class BlockTemporalCauldron extends BlockCauldron {
         this.setRegistryName(name);
         this.setUnlocalizedName(name);
         this.setCreativeTab(TimeTravelMod.tabTTM);
+    }
+
+    /**
+     * Called When an Entity Collided with the Block
+     */
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) { }
+
+    /**
+     * Called when the block is right clicked by a player.
+     */
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return false;
+    }
+
+    /**
+     * Called similar to random ticks, but only when it is raining.
+     */
+    @Override
+    public void fillWithRain(World worldIn, BlockPos pos) { }
+
+    /**
+     * Get the Item that this Block should drop when harvested.
+     */
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return Item.getItemFromBlock(ModBlocks.temporalCauldron);
+    }
+
+    @Override
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
+    {
+        return new ItemStack(Item.getItemFromBlock(ModBlocks.temporalCauldron));
     }
 }
