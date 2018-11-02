f6fb3c8e67ff2030e78dbc21ff7631587a23e244
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2018-11-02 17:19:10 +0100

    Attach the IItemHandler capability to the Temporal Cauldron TileEntity

--- src/main/java/com/rdvdev2/TimeTravelMod/common/block/tileentity/TileEntityTemporalCauldron.java
+++ src/main/java/com/rdvdev2/TimeTravelMod/common/block/tileentity/TileEntityTemporalCauldron.java
@@ -4,15 +4,34 @@ import com.rdvdev2.TimeTravelMod.ModBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nullable;
 
 public class TileEntityTemporalCauldron extends TileEntity implements ITickable {
+
+    @CapabilityInject(IItemHandler.class)
+    static Capability<IItemHandler> ITEM_HANDLER_CAPABILITY = null;
+
+    IItemHandler inventory = new ItemStackHandler(2) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            super.onContentsChanged(slot);
+            markDirty();
+        }
+    };
 
     @Override
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
+        ITEM_HANDLER_CAPABILITY.readNBT(inventory, null, compound.getTag("inventory"));
 
         // Read data
     }
@@ -20,6 +39,7 @@ public class TileEntityTemporalCauldron extends TileEntity implements ITickable 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         compound = super.writeToNBT(compound);
+        compound.setTag("inventory", ITEM_HANDLER_CAPABILITY.writeNBT(inventory, null));
 
         // Write data
 
@@ -51,5 +71,22 @@ public class TileEntityTemporalCauldron extends TileEntity implements ITickable 
     @Override
     public void handleUpdateTag(NBTTagCompound tag) {
         readFromNBT(tag);
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
+        if (capability == ITEM_HANDLER_CAPABILITY) {
+            return true;
+        }
+        return super.hasCapability(capability, facing);
+    }
+
+    @Nullable
+    @Override
+    public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
+        if (capability == ITEM_HANDLER_CAPABILITY) {
+            return (T) inventory;
+        }
+        return super.getCapability(capability, facing);
     }
 }
