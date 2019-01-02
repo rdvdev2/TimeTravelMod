70748bac24b41f89e53e647a1b78fd5705c30611
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2019-01-02 03:25:29 +0100

    Actually register the OpenTMGUI packet. No clue on how was this working before.

--- src/main/java/tk/rdvdev2/TimeTravelMod/common/networking/OpenTMGUI.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/common/networking/OpenTMGUI.java
@@ -2,15 +2,11 @@ package tk.rdvdev2.TimeTravelMod.common.networking;
 
 import com.google.common.base.Charsets;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import tk.rdvdev2.TimeTravelMod.ModRegistries;
 import tk.rdvdev2.TimeTravelMod.TimeTravelMod;
 import tk.rdvdev2.TimeTravelMod.api.timemachine.TimeMachine;
@@ -18,10 +14,11 @@ import tk.rdvdev2.TimeTravelMod.api.timemachine.upgrade.TimeMachineHookRunner;
 import tk.rdvdev2.TimeTravelMod.util.CastingHelper;
 
 public class OpenTMGUI implements IMessage {
+    public OpenTMGUI(){}
 
-    TimeMachine tm;
-    BlockPos pos;
-    EnumFacing side;
+    public TimeMachine tm;
+    public BlockPos pos;
+    public EnumFacing side;
 
     public OpenTMGUI(TimeMachine tm, BlockPos pos, EnumFacing side) {
         this.tm = tm instanceof TimeMachineHookRunner ? ((TimeMachineHookRunner)tm).removeHooks() : tm;
@@ -48,12 +45,10 @@ public class OpenTMGUI implements IMessage {
         buf.writeInt(side.getIndex());
     }
 
-    @SideOnly(Side.CLIENT)
     public static class OpenTMGUIHandler implements IMessageHandler<OpenTMGUI, IMessage> {
         @Override
         public IMessage onMessage(OpenTMGUI message, MessageContext ctx) {
-            EntityPlayer player = Minecraft.getMinecraft().player;
-            TimeTravelMod.proxy.displayTMGuiScreen(player, message.tm.hook(player.world, message.pos, message.side), message.pos, message.side);
+            TimeTravelMod.proxy.handleOpenTMGUI(message, ctx);
             return null;
         }
     }
