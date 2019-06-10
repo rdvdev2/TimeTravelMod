d22c5e79aa07993908dda4e74a5fb98ec4f2e746
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2019-06-10 23:53:52 +0200

    1.14.2 update (2nd stage) - Doesn't compile, but we discovered thread safety

--- src/main/java/tk/rdvdev2/TimeTravelMod/proxy/ClientProxy.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/proxy/ClientProxy.java
@@ -3,7 +3,10 @@ package tk.rdvdev2.TimeTravelMod.proxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.concurrent.ThreadTaskExecutor;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.LogicalSidedProvider;
 import net.minecraftforge.fml.network.NetworkEvent;
 import tk.rdvdev2.TimeTravelMod.TimeTravelMod;
 import tk.rdvdev2.TimeTravelMod.api.timemachine.TimeMachine;
@@ -14,7 +17,8 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void displayTMGuiScreen(PlayerEntity player, TimeMachine tm, BlockPos pos, Direction side) {
-        Minecraft.getInstance().addScheduledTask(()->Minecraft.getInstance().displayGuiScreen(new GuiTimeMachine(player, tm, pos, side))); // TODO: Thread safety?
+        LogicalSidedProvider.WORKQUEUE.<ThreadTaskExecutor<?>>get(LogicalSide.CLIENT).func_213165_a(()->Minecraft.getInstance().displayGuiScreen(new GuiTimeMachine(player, tm, pos, side))); // TODO: Check if this works
+        //Minecraft.getInstance().addScheduledTask(()->Minecraft.getInstance().displayGuiScreen(new GuiTimeMachine(player, tm, pos, side)));
     }
 
     @Override
