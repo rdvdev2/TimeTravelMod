81fde94679b9e2f00c880d6bf47ef5fea694bd3d
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2019-07-06 20:31:59 +0200

    Remove boilerplate code for items

--- src/main/java/tk/rdvdev2/TimeTravelMod/ModItems.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/ModItems.java
@@ -4,17 +4,14 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import tk.rdvdev2.TimeTravelMod.common.item.ControllerCircuitItem;
 import tk.rdvdev2.TimeTravelMod.common.item.CreativeTimeMachineItem;
-import tk.rdvdev2.TimeTravelMod.common.item.HeavyIngotItem;
-import tk.rdvdev2.TimeTravelMod.common.item.TimeCrystalItem;
 
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class ModItems {
 
-    public static Item timeCrystal = new TimeCrystalItem();
-    public static Item controllerCircuit = new ControllerCircuitItem();
-    public static Item heavyIngot = new HeavyIngotItem();
+    public static Item timeCrystal = new Item(new Item.Properties().group(TimeTravelMod.tabTTM)).setRegistryName("timecrystal");
+    public static Item controllerCircuit = new Item(new Item.Properties().group(TimeTravelMod.tabTTM)).setRegistryName("controllercircuit");
+    public static Item heavyIngot = new Item(new Item.Properties().group(TimeTravelMod.tabTTM)).setRegistryName("heavyingot");
     public static Item creativeTimeMachine = new CreativeTimeMachineItem();
 
     @SubscribeEvent
