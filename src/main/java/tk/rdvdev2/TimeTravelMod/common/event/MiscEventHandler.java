04a244fb75f1300fa4214e230c35a94127cfc2d9
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2019-08-10 19:11:51 +0200

    Update to forge .45 (Closes #56)

--- src/main/java/tk/rdvdev2/TimeTravelMod/common/event/MiscEventHandler.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/common/event/MiscEventHandler.java
@@ -2,11 +2,11 @@ package tk.rdvdev2.TimeTravelMod.common.event;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.VersionChecker;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import tk.rdvdev2.TimeTravelMod.ModConfig;
 import tk.rdvdev2.TimeTravelMod.TimeTravelMod;
 
