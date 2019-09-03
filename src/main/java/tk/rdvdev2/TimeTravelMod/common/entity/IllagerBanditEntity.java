81332d0a49ecfe2805f35433062e058a36cd614d
Roger Díaz Viñolas <rdv.dev2@gmail.com>
2019-09-03 19:30:10 +0200

    First steps on bandit's AI

--- src/main/java/tk/rdvdev2/TimeTravelMod/common/entity/IllagerBanditEntity.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/common/entity/IllagerBanditEntity.java
@@ -1,8 +1,14 @@
 package tk.rdvdev2.TimeTravelMod.common.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.RandomWalkingGoal;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.pathfinding.PathNavigator;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
@@ -15,5 +21,25 @@ public class IllagerBanditEntity extends MonsterEntity {
     @Override
     public boolean canSpawn(IWorld worldIn, SpawnReason spawnReasonIn) {
         return super.canSpawn(worldIn, spawnReasonIn);
+    }
+
+    @Override
+    protected PathNavigator createNavigator(World worldIn) {
+        PathNavigator navigator = super.createNavigator(worldIn);
+        navigator.setCanSwim(true);
+        return navigator;
+    }
+
+    @Override
+    protected void registerAttributes() {
+        super.registerAttributes();
+        this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(7, new RandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(7, new AvoidEntityGoal(this, IronGolemEntity.class, 5, 1.0D, 1.2D));
     }
 }
