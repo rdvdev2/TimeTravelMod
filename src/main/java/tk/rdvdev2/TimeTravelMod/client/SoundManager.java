8a405399b57b6b4fb381334f7bf80ee24e12bdba
Roger <rdvdev2@gmail.com>
2020-01-02 21:07:46 +0100

    Old West music is now quiter and doesn't overlap

--- src/main/java/tk/rdvdev2/TimeTravelMod/client/SoundManager.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/client/SoundManager.java
@@ -1,6 +1,7 @@
 package tk.rdvdev2.TimeTravelMod.client;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.SimpleSound;
 import net.minecraft.util.SoundCategory;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
@@ -9,12 +10,19 @@ import tk.rdvdev2.TimeTravelMod.ModSounds;
 import tk.rdvdev2.TimeTravelMod.common.world.dimension.oldwest.OldWestDimension;
 
 public class SoundManager {
+
+    private static final ISound OLDWEST_MUSIC = SimpleSound.music(ModSounds.OLDWEST_MUSIC);
+
     public static void onPlaySound(PlaySoundEvent event) {
         if (ModConfig.CLIENT.enableTimeLineMusic.get()) {
             if (Minecraft.getInstance().player != null &&
                     Minecraft.getInstance().player.world.getDimension() instanceof OldWestDimension &&
                     event.getSound().getCategory() == SoundCategory.MUSIC) {
-                event.setResultSound(SimpleSound.music(ModSounds.OLDWEST_MUSIC));
+                if (Minecraft.getInstance().getSoundHandler().isPlaying(OLDWEST_MUSIC)) {
+                    event.setResultSound(null);
+                } else {
+                    event.setResultSound(OLDWEST_MUSIC);
+                }
             }
         }
     }
