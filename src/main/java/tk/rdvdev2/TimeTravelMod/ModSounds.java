51d8ce802a9cee7dbe3769b62a651fac1f032c66
rdvdev2 <rdvdev2@gmail.com>
2020-02-08 21:25:12 +0100

    Remove unused EventBusSubscriber

--- src/main/java/tk/rdvdev2/TimeTravelMod/ModSounds.java
+++ src/main/java/tk/rdvdev2/TimeTravelMod/ModSounds.java
@@ -3,14 +3,12 @@ package tk.rdvdev2.TimeTravelMod;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import static tk.rdvdev2.TimeTravelMod.TimeTravelMod.MODID;
 
-@Mod.EventBusSubscriber(bus= Mod.EventBusSubscriber.Bus.MOD)
 public class ModSounds {
 
     private static final DeferredRegister<SoundEvent> SOUND_EVENTS = new DeferredRegister<>(ForgeRegistries.SOUND_EVENTS, MODID);
